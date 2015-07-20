abe61f290d843cb4c2c0c444bb4936bfe0d00eb1
jocelyn alsdorf & jeanette fairless <student@epicodus.com>
2015-07-20 14:25:46 -0700

    add contacts model and set up associations between messages and contacts

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720181438) do
+ActiveRecord::Schema.define(version: 20150720212413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "messages", force: :cascade do |t|
-    t.string "to"
-    t.string "from"
-    t.string "body"
+    t.string  "to"
+    t.string  "from"
+    t.string  "body"
+    t.integer "contact_id"
   end
 
 end
